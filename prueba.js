1021a8d7ebbc94714ee8131d9979e77641b13051
AlexDemayo <alexd_91@hotmail.com>
2020-04-22 11:36:27 -0300

    cambios en prueba.js

--- prueba.js
+++ prueba.js
@@ -2,3 +2,11 @@ for (let index = 0; index < 5; index++) {
     console.log('hola vez' + i )
     
 }
+console.log(' aca se va a complicar ');
+
+
+
+for (let index = 0; index < 5; index++) {
+    console.log('hola  2 vez' + i )
+    
+}
